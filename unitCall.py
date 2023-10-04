a3a5374b3dae503d97dd17e6a07437b77135a37f
gmkung <guangmian@gmail.com>
2023-10-04 23:24:47 +0800

    Something

--- unitCall.py
+++ unitCall.py
@@ -1,7 +1,7 @@
 from policebot import handle_event
 
-itemId = "0x9c0744cc8a74bce1d14f038c951cf82564e5abb4e31af6c1998021fa5eb2ddb8"
-ipfsUri = "/ipfs/Qmb5vWHTvFP6DuyrGGdix6PXmmK881Fh7XrXcdraniQW6Y/item.json"
+itemId = "0xdb3d890c18c324d82bad11d10906dedadd5eb654227e97603f25730e0362fad3"
+ipfsUri = "/ipfs/QmdBp8CsWeDjy3wzDw4F2hV5JoTX4AqR8NwqzEGqNG6VLf/item.json"
 
 
 bytes_object = bytes.fromhex(itemId[2:])
